676905c005c81d65113d6902c7d85416d0ba28c0
Phil Sutter <psutter@redhat.com>
2025-07-10 17:45:54 +0200

    fixup! lnst: Recipes: Introduce NftablesRuleScaleRecipe

--- lnst/Recipes/ENRT/NftablesRuleScaleRecipe.py
+++ lnst/Recipes/ENRT/NftablesRuleScaleRecipe.py
@@ -1,10 +1,16 @@
 from lnst.Recipes.ENRT.SimpleNetnsRouterRecipe import SimpleNetnsRouterRecipe
 from lnst.Recipes.ENRT.ConfigMixins.FirewallMixin import NftablesMixin
-from lnst.Recipes.ENRT.MeasurementGenerators.FlowMeasurementGenerator import FlowMeasurementGenerator
 from lnst.Common.Parameters import StrParam, IntParam, BoolParam
 
-class NftablesRuleScaleRecipe(SimpleNetnsRouterRecipe, NftablesMixin, FlowMeasurementGenerator):
+class NftablesRuleScaleRecipe(SimpleNetnsRouterRecipe, NftablesMixin):
     """
+    This recipe combines SimpleNetnsRouterRecipe and NftablesMixin for testing
+    routing throughput impact of specific nftables rules. To generate
+    meaningful results, the rule is simply repeated multiple times, thereby
+    amplifying the impact.
+    Rules are added to host2's forwarding hook which routes test traffic
+    between host1 and a local netns.
+
     :param rule:
         The actual rule to insert repeatedly into the router's forwarding
         chain.
@@ -24,15 +30,14 @@ class NftablesRuleScaleRecipe(SimpleNetnsRouterRecipe, NftablesMixin, FlowMeasur
 
     @property
     def firewall_rulesets_generator(self):
-        rule = self.params.get('rule', None)
-        scale = self.params.get('scale', None)
+        rule = self.params.get('rule')
+        scale = self.params.get('scale')
         ruleset = [
             "flush ruleset",
             "add table inet t",
             "add chain inet t forward { type filter hook forward priority filter; }",
-        ]
-        ruleset.extend([f"add rule inet t forward {rule}" for s in range(scale)])
-        if self.params.get('flowtable', None):
+        ] + [f"add rule inet t forward {rule}"] * scale
+        if self.params.get('flowtable'):
             devs = f"{self.matched.host2.eth0.name}, {self.matched.host2.pn0.name}"
             ftspec = f"hook ingress priority filter; devices = {{ {devs} }};"
             ruleset.append(f"add flowtable inet t ft {{ {ftspec} }}")
@@ -40,11 +45,11 @@ class NftablesRuleScaleRecipe(SimpleNetnsRouterRecipe, NftablesMixin, FlowMeasur
         yield { self.matched.host2: "\n".join(ruleset) }
 
     def generate_sub_configuration_description(self, config):
-        rule = self.params.get('rule', None)
-        scale = self.params.get('scale', None)
+        rule = self.params.get('rule')
+        scale = self.params.get('scale')
         desc = super().generate_sub_configuration_description(config)
         msg = f"NftablesScale: Ruleset with {scale} times '{rule}'"
-        if self.params.get('flowtable', None):
+        if self.params.get('flowtable'):
             msg += " and a flowtable"
         desc.append(msg)
         return desc
